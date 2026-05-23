69549b7c6f178798dc9b00244912fe04ef5a8396
Adriana Umaña <naniz20020@gmail.com>
2026-05-22 20:43:18 -0500

    Matriz con los datos de los clientes

---
+++ 213022_973 _Fase5_AdrianaUmaña.py
@@ -0,0 +1,56 @@
+# ===========================================
+# Clasificacion de compromiso de clientes
+# ===========================================
+
+
+# Matriz con los datos de los clientes
+# [ID Cliente, Duracion en segundos, Eventos clics]
+
+Clientes = [
+    ["C001", 250, 10],
+    ["C002", 45, 2],
+    ["C003", 120, 5],
+    ["C004", 190, 4],
+    ["C005", 300, 12]
+]
+
+# Mostrar datos almacenados
+print("Datos de las sesiones")
+print("-----------------------------------------")
+
+for cliente in Clientes:
+    print("ID:", cliente[0],
+          "| Duracion:", cliente[1],"seg",
+          "| Clics:", cliente[2])
+    
+print("\n")
+
+
+# Funcion para clasificar el compromiso
+
+def clasificar_compromiso(duracion, clics):
+    
+    if duracion > 180 and clics > 8:
+        return "Alto"
+    elif duracion < 60 or clics < 3:
+        return "Bajo"
+    else:
+        return "Medio"
+    
+# Informe final
+print("Informe Final De Clientes")
+print("--------------------------------------")
+
+# Motrar lista final con resultados
+
+for cliente in Clientes:
+    
+    id_cliente = cliente[0]
+    duracion = cliente[1]
+    clics = cliente[2]
+    
+    clasificacion = clasificar_compromiso(duracion, clics)
+    
+    print("Cliente:", id_cliente,
+          "| Clasificacion:", clasificacion)
+    
